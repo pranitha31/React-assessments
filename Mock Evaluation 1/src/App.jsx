8eea3fe20f73e7cf17e6bbe182d737ec285aeb45
pranitha <chikkelapranitha@gmail.com>
2025-12-21 19:21:40 +0530

    mainand css added

--- Mock Evaluation 1/src/App.jsx
+++ Mock Evaluation 1/src/App.jsx
@@ -1,1 +1,15 @@
+import ThemeToggle from "./components/ThemeToggle";
+import { useTheme } from "./contexts/ThemeContext";
+import PostList from "./components/PostList";
 
+const App=()=>{
+    const {theme}=useTheme();
+    return(
+        <div className="{`app ${theme}`}">
+            <h1>Post Manager</h1>
+            <ThemeToggle/>
+            <PostList/>
+        </div>
+    );
+};
+export default App;
--- Mock Evaluation 1/src/main.jsx
+++ Mock Evaluation 1/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDom from "react-dom/client";
+import App from "./App";
+import {PostProvider} from "./contexts/PostContext";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import "./Styles.css"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDom.createRoot(document.getElementById("root")).render(
+  <ThemeProvider>
+    <PostProvider>
+      <App />
+    <PostProvider>
+  <ThemeProvider>
+);
